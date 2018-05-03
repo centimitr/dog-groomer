98323a12e21c71e9a442eb5bc4155ee90ee16eb7
Centimitr <centimitr@gmail.com>
2018-05-03 18:24:18 +1000

    preview login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBeSIU5-_JDMIwv5sMzDmqDZdS-aBTL32E',
+    authDomain: 'swen90016-67ebf.firebaseapp.com',
+    databaseURL: 'https://swen90016-67ebf.firebaseio.com',
+    projectId: 'swen90016-67ebf',
+    // storageBucket: 'swen90016-67ebf.appspot.com',
+    // messagingSenderId: '543963271783'
+  }
+}
